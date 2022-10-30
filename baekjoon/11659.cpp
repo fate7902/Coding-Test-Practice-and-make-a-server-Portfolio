ed0d1d1b64ac34fe9f8902e685ad2a5eeedfc338
박의인 <fate7902@naver.com>
2022-10-31 01:58:34 +0900

    baekjoon 12 questions

---
+++ baekjoon/11659.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+vector<int> arr;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m, temp, sum = 0;
+    cin >> n >> m;
+    arr.emplace_back(0);
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        sum += temp;
+        arr.emplace_back(sum);
+    }
+    int a, b;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        cout << arr[b] - arr[a - 1] << "\n";
+    }
+}
